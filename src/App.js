e4238aea8b191761fdfb48ee0437ee74b9121338
saubhagya <saubhagya07@yahoo.com>
2022-09-14 18:49:37 +0530

    Update App.js
    footer1 imported

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 
 import Header from "./components/Header/Header";
 import Body from "./components/Body/Body";
-import Footer from "./components/footer/Footer";
+//import Footer from "./components/footer/Footer";
 import "./App.css";
+import Footer1 from './components/footer/footer1';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Body />
-      <Footer />
+      <Footer1 />
     </div>
   );
 }
