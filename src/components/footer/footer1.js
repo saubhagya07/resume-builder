fdf64d201f382d0371efd32841249ae01f2a5b91
saubhagya <saubhagya07@yahoo.com>
2022-09-14 18:47:56 +0530

    Add files via upload
    new footer added

---
+++ src/components/footer/footer1.js
@@ -0,0 +1,107 @@
+import React from "react";
+const Footer1 = () => {
+    return (
+        <footer>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-lg-10 mx-auto">
+                        <div className="row">
+
+                                    <div className="col-6 col-lg-3">
+                                        <h4>About-us1</h4>
+                                        <ul>
+                                            <li>
+                                                LINE 1
+                                            </li>
+                                            <li>
+                                                LINE2
+                                            </li>
+                                            <li>
+                                                LINE 3
+                                            </li>
+                                            <li>
+                                                LINE 4
+                                            </li>
+                                        </ul>
+                                    </div>
+
+                                    <div className="col-6 col-lg-3">
+                                        <h4>About-us2</h4>
+                                        <ul>
+                                            <li>
+                                                LINE 1
+                                            </li>
+                                            <li>
+                                                LINE 2
+                                            </li>
+                                            <li>
+                                                LINE 3
+                                            </li>
+                                            <li>
+                                                LINE4
+                                            </li>
+                                        </ul>
+                                    </div>
+
+                                    <div className="col-6 col-lg-3">
+                                        <h4>About-us3</h4>
+                                        <ul>
+                                            <li>
+                                                <a href="#">LINK1</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">LINK2</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">LINK3</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">LINK4</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+
+                                    <div className="col-6 col-lg-3">
+                                            <h4>Follow Us</h4>
+                                            <div className="row">
+                                                <div className="col-3 mx-auto">
+                                                <a href="https://www.instagram.com/accounts/onetap/?next=https%3A%2F%2Fwww.instagram.com%2F%3F__coig_login%3D1">
+                                                <i class="fab fa-instagram fontawesome-style"></i>
+                                                </a>
+                                                </div>
+                                            </div>
+
+                                            <div className="row">
+                                                <div className="col-3 mx-auto">
+                                                <a href="#">
+                                                <i class="fab fa-instagram fontawesome-style"></i>
+                                                </a>
+                                                </div>
+                                            </div>
+
+                                            <div className="row">
+                                                <div className="col-3 mx-auto">
+                                                
+                                                <i class="fab fa-instagram fontawesome-style"></i>
+                                                
+                                                </div>
+                                            </div>
+                                    </div>
+
+                                              
+
+                        </div>
+
+                                <hr />
+                                    <div className="mt-5">
+                                    <p className="main-hero-para text-center w-100">copyright @ 2022 Rockstar. All right reserved</p>
+
+                                    </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer1;
